684010a175842fee1817517144015c4433678d37
Belal Ahmad <belalahmad.fleapo@gmail.com>
2023-04-29 02:42:40 +0530

    adding codes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { OrdersModule } from './orders/orders.module';
+import { CheckoutModule } from './checkout/checkout.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, WishlistModule, OrdersModule, CheckoutModule],
   controllers: [AppController],
   providers: [AppService],
 })
