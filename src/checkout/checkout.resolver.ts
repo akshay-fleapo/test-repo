b63331aef95dd94ecc3743a2b7ed5c2b1d566b83
Akshay Rajput <akshay@fleapo.com>
2023-05-26 00:13:28 +0530

    Resolved Conflicts

--- src/checkout/checkout.resolver.ts
+++ src/checkout/checkout.resolver.ts
@@ -1,35 +1,34 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { Checkout } from "./entity/checkout.entity";
-import { CheckoutService } from "./checkout.service";
-import { GqlAuthGuard } from "src/auth/guards";
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Checkout } from './entity/checkout.entity';
+import { CheckoutService } from './checkout.service';
+import { GqlAuthGuard } from 'src/auth/guards';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { IJwtPayload } from 'src/auth/dto/jwt-payload.interface';
 import { CreateCheckoutDto } from './dto/create-checkout.dto';
 
 @Resolver(() => Checkout)
 export class CheckoutResolver {
-    constructor(private readonly checkOutService: CheckoutService) { }
+  constructor(private readonly checkOutService: CheckoutService) {}
 
-    @UseGuards(GqlAuthGuard)
-    @Query(() => Checkout)
-    async getCheckout(@CurrentUser() user: IJwtPayload) {
-        return this.checkOutService.getCheckout(user)
-    }
+  @UseGuards(GqlAuthGuard)
+  @Query(() => Checkout)
+  async getCheckout(@CurrentUser() user: IJwtPayload) {
+    return this.checkOutService.getCheckout(user);
+  }
 
-    @UseGuards(GqlAuthGuard)
-    @Mutation(() => Checkout)
-    async createCheckout(
-        @CurrentUser() user: IJwtPayload,
-        @Args('createCheckoutInput') createCheckoutDto: CreateCheckoutDto
-    ) {
-        return this.checkOutService.createCheckout(user, createCheckoutDto);
-    }
+  @UseGuards(GqlAuthGuard)
+  @Mutation(() => Checkout)
+  async createCheckout(
+    @CurrentUser() user: IJwtPayload,
+    @Args('createCheckoutInput') createCheckoutDto: CreateCheckoutDto
+  ) {
+    return this.checkOutService.createCheckout(user, createCheckoutDto);
+  }
 
-    @UseGuards(GqlAuthGuard)
-    @Mutation(() => Checkout)
-    async deleteCheckout(@CurrentUser() user: IJwtPayload, @Args('id') id: string) {
-        return await this.checkOutService.deleteCheckout(user, id);
-    }
-
+  @UseGuards(GqlAuthGuard)
+  @Mutation(() => Checkout)
+  async deleteCheckout(@CurrentUser() user: IJwtPayload, @Args('id') id: string) {
+    return await this.checkOutService.deleteCheckout(user, id);
+  }
 }
--- src/checkout/checkout.service.ts
+++ src/checkout/checkout.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Checkout } from './entity/checkout.entity';
 import { Repository } from 'typeorm';
@@ -7,42 +7,38 @@ import { CreateCheckoutDto } from './dto/create-checkout.dto';
 
 @Injectable()
 export class CheckoutService {
-    constructor(
-        @InjectRepository(Checkout)
-        private readonly checkoutRepository: Repository<Checkout>
-    ) { }
+  constructor(
+    @InjectRepository(Checkout)
+    private readonly checkoutRepository: Repository<Checkout>
+  ) {}
 
-    async getCheckout(user: IJwtPayload) {
-        const foundCheckouts = await this.checkoutRepository.findOne({
-            where: { gifter: { id: user.id } },
-            relations: ['user']
-        });
-        return foundCheckouts
-    }
+  async getCheckout(user: IJwtPayload) {
+    const foundCheckouts = await this.checkoutRepository.findOne({
+      where: { gifter: { id: user.id } },
+      relations: ['user']
+    });
+    return foundCheckouts;
+  }
 
+  async createCheckout(user: IJwtPayload, createCheckoutDto: CreateCheckoutDto): Promise<Checkout> {
+    const { productId } = createCheckoutDto;
+    const foundData = await this.checkoutRepository.findOne({
+      where: { gifter: { id: user.id }, product: { id: productId } }
+    });
+    if (foundData)
+      throw new NotFoundException(`checkout item with gifter ${user.id} and productId ${productId} already exists`);
 
-    async createCheckout(user: IJwtPayload, createCheckoutDto: CreateCheckoutDto): Promise<Checkout> {
-        const {productId }= createCheckoutDto;
-        const foundData = await this.checkoutRepository.findOne({
-            where: { gifter: { id: user.id }, product: { id: productId } }
-        });
-        if (foundData)
-            throw new NotFoundException(
-                `checkout item with gifter ${user.id} and productId ${productId} already exists`
-            );
+    const checkoutItem = await this.checkoutRepository.create({
+      ...createCheckoutDto,
+      gifter: { id: user.id },
+      product: { id: productId }
+    });
+    return await this.checkoutRepository.save(checkoutItem);
+  }
 
-        const checkoutItem = await this.checkoutRepository.create({
-            ...createCheckoutDto,
-            gifter: { id: user.id },
-            product: {id: productId}
-        });
-        return await this.checkoutRepository.save(checkoutItem);
-    }
-
-    async deleteCheckout(user: IJwtPayload, id: string) {
-        const checkout = await this.checkoutRepository.findOne({ where: { id, gifter: { id: user.id } } });
-        if (!checkout) throw new NotFoundException('Checkout Item not found');
-        return await this.checkoutRepository.save({ ...checkout });
-      }
-
+  async deleteCheckout(user: IJwtPayload, id: string) {
+    const checkout = await this.checkoutRepository.findOne({ where: { id, gifter: { id: user.id } } });
+    if (!checkout) throw new NotFoundException('Checkout Item not found');
+    return await this.checkoutRepository.save({ ...checkout });
+  }
 }
